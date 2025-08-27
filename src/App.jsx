f70a13ed35b7f14db158df661e471caca7cd495f
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-08-27 16:08:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import useSwitch from "./hook/useSwith";
+export default function App() {
+  const [ isOn, toggle ] = useSwitch();
+  return (
+    <div>
+      <h1>Il valore è: {isOn ? "On" : "Off"}</h1>
+      <button onClick={toggle}>Cambia stato</button>
+    </div>
+   
+  )
+};
+
+ 
